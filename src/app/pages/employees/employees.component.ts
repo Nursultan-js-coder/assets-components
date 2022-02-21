3e0a8de5d7721bbf843d1202446e10f49420bbcc
Lukmanov <ozggldd@gmail.com>
2022-02-21 14:44:28 +0600

    init commit

--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
 })
 export class EmployeesComponent implements OnInit {
-
+  nameControl = new FormControl();
   constructor() { }
 
   ngOnInit(): void {
