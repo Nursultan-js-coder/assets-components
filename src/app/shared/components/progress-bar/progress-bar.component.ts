7fdd9fcec38661e2754a9f3b03e7f8b8e818c567
Lukmanov <ozggldd@gmail.com>
2022-03-07 10:25:18 +0600

    fix

--- src/app/shared/components/progress-bar/progress-bar.component.ts
+++ src/app/shared/components/progress-bar/progress-bar.component.ts
@@ -3,37 +3,52 @@ import {MatTabChangeEvent} from "@angular/material/tabs";
 import {FormBuilder, FormControl, Validators} from "@angular/forms";
 import {EmployeeForm} from "@app/shared/components/progress-bar/EmployeeForm";
 
+interface ITab {
+  hint?: string,
+  index: number
+}
+
 @Component({
-    selector: 'app-progress-bar',
-    templateUrl: './progress-bar.component.html',
-    styleUrls: ['./progress-bar.component.scss']
+  selector: 'app-progress-bar',
+  templateUrl: './progress-bar.component.html',
+  styleUrls: ['./progress-bar.component.scss']
 })
 export class ProgressBarComponent implements OnInit {
-    selectedIndex = 0;
-    totalTabCount = 3;
-    control = new FormControl();
-    control1 = new FormControl();
-    form: EmployeeForm;
+  selectedIndex = 1;
+  totalTabCount = 3;
+  control = new FormControl();
+  control1 = new FormControl();
+  form: EmployeeForm;
+  tabs: ITab[] = [];
+  currentTab: ITab;
 
-    constructor() {
-        this.control.addValidators(Validators.required)
-        this.control1.addValidators(Validators.required)
-        this.form = new EmployeeForm(null, new FormBuilder());
-    }
+  constructor() {
+    this.control.addValidators(Validators.required)
+    this.control1.addValidators(Validators.required)
+    this.form = new EmployeeForm(null, new FormBuilder());
+    this.tabs = [
+      {index: 0},
+      {index: 1},
+      {index: 2, hint: "Вы можете пропустить этот шаг и добавить информацию позже!"},
+    ]
+    this.currentTab = this.tabs[this.selectedIndex ?? 0];
+  }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {
+  }
 
 
-    onSelectedTabChange(event: MatTabChangeEvent) {
-        this.selectedIndex = event.index;
-    }
+  onSelectedTabChange(event: MatTabChangeEvent) {
+    this.selectedIndex = event.index;
+    this.currentTab = this.tabs[this.selectedIndex];
+  }
 
-    get processPercentage() {
-        return Math.max(1, this.selectedIndex + 1) / this.totalTabCount * 100;
-    }
 
-    onSubmit() {
-        console.log("On submit:", this.form.value);
-    }
+  get processPercentage() {
+    return Math.max(1, this.selectedIndex + 1) / this.totalTabCount * 100;
+  }
+
+  onSubmit() {
+    console.log("On submit:", this.form.value);
+  }
 }
